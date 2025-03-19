04c2d518229a6c55c3da2c7f2ea41cdc092955fe
Prateek jain <prateek.jn624@gmail.com>
2025-03-19 22:42:26 +0530

    added vercel.json file

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,4 +1,11 @@
 import { createContext, useContext } from "react";
+import { auth } from "../Firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 export const AuthContext = createContext(null);
 
@@ -8,5 +15,13 @@ export const useAuthContext = () => {
 
 // Create a context for authentication status and functions to manage it.
 export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const signup = async (email, password) =>
+    createUserWithEmailAndPassword(email, password);
+
+  const signin = async (email, password) =>
+    signInWithEmailAndPassword(email, password);
+
   return <AuthContext.Provider>{children}</AuthContext.Provider>;
 };
